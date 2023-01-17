207e83b3a8177bea4f3b0326417c5bb7b1d14d23
kiaella <emmanuellaodoemenan9@gmail.com>
2023-01-17 15:06:15 -0800

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,16 +3,16 @@
 
 /**
 * struct dog - structure for a basic dog
-* @name: Dog's name
-* @age: Dog's age
-* @owner: Dog's owner
-* Description: data structure for dog
-*/
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
